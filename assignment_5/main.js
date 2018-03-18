0af4d044cb87ceed9dcab59855e4b55e8e21f9dc
tvondavi <tvondavi@andrew.cmu.edu>
2018-03-18 19:23:32 -0400

    something is working

--- assignment_5/main.js
+++ assignment_5/main.js
@@ -1,5 +1,1 @@
-var x = document.getElementById("banana");
-console.log(x);
-
-// var chosenOption = chosen.options[chosen.selectedIndex].value;
-// console.log(chosenOption);
+var x = document.getElementsByClassName("Glazes");
