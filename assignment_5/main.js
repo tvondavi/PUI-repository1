97039a3f89630f6fd30fec9d4c591f40db44ad1e
tvondavi <tvondavi@andrew.cmu.edu>
2018-03-25 13:40:08 -0400

    more work with js

--- assignment_5/main.js
+++ assignment_5/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
 //I created an empty object that will be manipulated later 
     var bun = [];
 
@@ -12,6 +11,8 @@ $(document).ready(function(){
         console.log(bun);
         //Save the now filled object in local storage for page manipulation
         localStorage.setItem("buns", JSON.stringify(bun));
+        //This updates the website's description of the detail page.
+        $('#text').append("You have selected " + $(this).val() + " buns without any glaze.");
         //I did the above things for all of the drop down bars
     });
     $('#sugarMilk').change(function(){
@@ -19,21 +20,33 @@ $(document).ready(function(){
         bun.push(sugarMilk);
         console.log(bun);
         localStorage.setItem("buns", JSON.stringify(bun));
+        $('#text').append("You have selected " + $(this).val() + " buns with sugar milk glaze.");
     });
     $('#vanillaMilk').change(function(){
     	var vanillaMilk = { glaze: "vanillaMilk", number: $(this).val() };
         bun.push(vanillaMilk);
         console.log(bun);
         localStorage.setItem("buns", JSON.stringify(bun));
+        $('#text').append("You have selected " + $(this).val() + " buns with vanilla milk glaze.");
     });
     $('#doubleChocoloate').change(function(){
     	var doubleChocoloate = { glaze: "doubleChocoloate", number: $(this).val() };
         bun.push(doubleChocoloate);
         console.log(bun);
         localStorage.setItem("buns", JSON.stringify(bun));
+        $('#text').append("You have selected " + $(this).val() + " buns with double chocolate glaze.");
     });
     
-    $("#checkout").onclick(function() {
-    	
-    }
+    $('#checkout').click(function() {
+        //check to see if there is something stored in localStorage
+        if (localStorage.getItem("buns") !== null){
+        	//Updates the basket list item when the user clicks on the add to basket button
+        	$('#notification').append(" (!)");
+
+        	$("#purchase").html(JSON.parse(bun));
+        }
+        else {
+        	$('#message').append("You don't have any buns in your basket!");
+        }
+    });
 });
