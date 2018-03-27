fd5d7239a39aed5d6841eb0e38be4b49f34688f1
tvondavi <tvondavi@andrew.cmu.edu>
2018-03-27 14:31:41 -0400

    things are working!

--- assignment_5/basket_page.js
+++ assignment_5/basket_page.js
@@ -13,7 +13,7 @@ function vanillaMilk(glaze, number){
 function sugarMilk(glaze, number){
 	this.glaze = glaze;
 	this.number = number;
-	this.image = "Assets/sugarMilk.jpg";
+	this.image = "Assets/sugarGlaze.jpg";
 }
 
 function doubleChocolate(glaze, number){
@@ -26,33 +26,36 @@ function doubleChocolate(glaze, number){
 var storedBun = localStorage.getItem("buns");
 var bunList = JSON.parse(storedBun);
 
-function identifyBun (){
-    for (i in bunList) {
-	    if (bunList[i].glaze == "none") {
-		    return new noGlaze(bunList[i].glaze, bunList[i].number);
-		    i += 1;
-	    }
-	    if (bunList[i].glaze == "vanillaMilk") {
-	    	return new vanillaMilk(bunList[i].glaze, bunList[i].number);
-	    	i += 1;
-	    }
-        if (bunList[i].glaze == "sugarMilk") {
-        	return new sugarMilk(bunList[i].glaze, bunList[i].number);
-        	i += 1;
-        }
-        if (bunList[i].glaze == "doubleChocolate") {
-    	    return new doubleChocolate(bunList[i].glaze, bunList[i].number);
-    	    i += 1;
-        }
+function identifyBun(){         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	if (bunList[i].glaze == "none") {
+	    return new noGlaze(bunList[i].glaze, bunList[i].number);
+	}
+	if (bunList[i].glaze == "vanillaMilk") {
+	   	return new vanillaMilk(bunList[i].glaze, bunList[i].number);
+	}
+    if (bunList[i].glaze == "sugarMilk") {
+      	return new sugarMilk(bunList[i].glaze, bunList[i].number);
+    }
+    if (bunList[i].glaze == "doubleChocolate") {
+        return new doubleChocolate(bunList[i].glaze, bunList[i].number);
     }
 }
 
+var newBuns = [];
+
+function createBun(){
+	for (i in bunList){
+		newBuns.push(identifyBun(bunList[i]));
+	}
+}
 
 $(document).ready(function(){
+	
+	createBun();
     
-    var selectedBun = identifyBun();
-
-	$('#bun_Description').text("You have selected " + selectedBun.number + " bun(s) with " + selectedBun.glaze + " style glaze.");
-	$('#bun_Image').attr("src", selectedBun.image);
-	$('#price').text("Price: $" + (1.50 * selectedBun.number));
+    for (var i = 0; i < newBuns.length; i ++){
+        $('.bun_Description').text("You have selected " + newBuns[i].number + " bun(s) with " + newBuns[i].glaze + " style glaze.");
+        $('.bun_Image').attr("src", newBuns[i].image);
+        $('.price').text("Price: $" + (1.50 * newBuns[i].number));
+    }
 });
